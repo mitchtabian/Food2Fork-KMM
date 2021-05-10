de3e920a64010fbbff11ec81b0d751725f0b4675
Mitch Tabian <mitch@tabian.ca>
2021-05-10 13:06:21 -0700

    start ktor demo

--- androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/MainActivity.kt
+++ androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/MainActivity.kt
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import com.codingwithmitch.food2forkkmm.android.presentation.navigation.Navigation
+import com.codingwithmitch.food2forkkmm.datasource.network.KtorClientFactory
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val ktorClient = KtorClientFactory().build()
+        val recipe = ktorClient.get{
+            url("$baseUrl/get?id=$id")
+            header("Authorization", TOKEN)
+        }
+
         setContent{
             Navigation()
         }
