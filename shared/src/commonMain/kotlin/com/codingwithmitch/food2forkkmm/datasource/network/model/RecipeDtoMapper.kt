adc62eddeb7d0796b00455b8da368b4fab5b6188
Mitch Tabian <mitch@tabian.ca>
2021-05-10 13:35:07 -0700

    Mapping to and from a Core Business Model

---
+++ shared/src/commonMain/kotlin/com/codingwithmitch/food2forkkmm/datasource/network/model/RecipeDtoMapper.kt
@@ -0,0 +1,45 @@
+package com.codingwithmitch.food2forkkmm.datasource.network.model
+
+import com.codingwithmitch.food2forkkmm.domain.model.Recipe
+import com.codingwithmitch.food2forkkmm.domain.util.DomainMapper
+
+class RecipeDtoMapper : DomainMapper<RecipeDto, Recipe> {
+
+    override fun mapToDomainModel(model: RecipeDto): Recipe {
+        return Recipe(
+            id = model.pk,
+            title = model.title,
+            featuredImage = model.featuredImage,
+            rating = model.rating,
+            publisher = model.publisher,
+            sourceUrl = model.sourceUrl,
+            ingredients = model.ingredients,
+            dateAdded = TODO("Date conversion"),
+            dateUpdated = TODO("Date conversion"),
+        )
+    }
+
+    override fun mapFromDomainModel(domainModel: Recipe): RecipeDto {
+        return RecipeDto(
+            pk = domainModel.id,
+            title = domainModel.title,
+            featuredImage = domainModel.featuredImage,
+            rating = domainModel.rating,
+            publisher = domainModel.publisher,
+            sourceUrl = domainModel.sourceUrl,
+            ingredients = domainModel.ingredients,
+            longDateAdded = TODO("Date conversion"),
+            longDateUpdated = TODO("Date conversion"),
+        )
+    }
+
+    fun toDomainList(initial: List<RecipeDto>): List<Recipe>{
+        return initial.map { mapToDomainModel(it) }
+    }
+
+    fun fromDomainList(initial: List<Recipe>): List<RecipeDto>{
+        return initial.map { mapFromDomainModel(it) }
+    }
+
+
+}
---
+++ shared/src/commonMain/kotlin/com/codingwithmitch/food2forkkmm/domain/util/DomainMapper.kt
@@ -0,0 +1,8 @@
+package com.codingwithmitch.food2forkkmm.domain.util
+
+interface DomainMapper <T, DomainModel>{
+
+    fun mapToDomainModel(model: T): DomainModel
+
+    fun mapFromDomainModel(domainModel: DomainModel): T
+}
