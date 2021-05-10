3d6f2728810bb57e607ae84c84d896667a5a9576
Mitch Tabian <mitch@tabian.ca>
2021-05-10 11:39:54 -0700

    Complete Hilt setup

---
+++ androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.codingwithmitch.food2forkkmm.android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application()
+
+
--- androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/MainActivity.kt
+++ androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/MainActivity.kt
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import com.codingwithmitch.food2forkkmm.android.presentation.navigation.Navigation
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
