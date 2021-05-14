c63ef5b8af95fd04729348114e88326ad488b76b
Mitch Tabian <mitch@tabian.ca>
2021-05-14 13:21:08 -0700

    refactor domain mapping strategy

--- shared/src/commonMain/kotlin/com/codingwithmitch/food2forkkmm/datasource/network/KtorClientFactory.kt
+++ shared/src/commonMain/kotlin/com/codingwithmitch/food2forkkmm/datasource/network/KtorClientFactory.kt
@@ -1,7 +1,27 @@
 package com.codingwithmitch.food2forkkmm.datasource.network
 
+import com.codingwithmitch.food2forkkmm.datasource.network.model.RecipeDto
+import com.codingwithmitch.food2forkkmm.domain.model.Recipe
 import io.ktor.client.*
 
 expect class KtorClientFactory() {
     fun build(): HttpClient
+}
+
+fun RecipeDto.toRecipe(): Recipe {
+    return Recipe(
+        id = pk,
+        title = title,
+        featuredImage = featuredImage,
+        rating = rating,
+        publisher = publisher,
+        sourceUrl = sourceUrl,
+        ingredients = ingredients,
+        dateAdded = , // ??
+        dateUpdated = , // ??
+    )
+}
+
+fun List<RecipeDto>.toRecipeList(): List<Recipe>{
+    return map{it.toRecipe()}
 }
--- shared/src/commonMain/kotlin/com/codingwithmitch/food2forkkmm/domain/util/DomainMapper.kt
+++
@@ -1,8 +0,0 @@
-package com.codingwithmitch.food2forkkmm.domain.util
-
-interface DomainMapper <T, DomainModel>{
-
-    fun mapToDomainModel(model: T): DomainModel
-
-    fun mapFromDomainModel(domainModel: DomainModel): T
-}
