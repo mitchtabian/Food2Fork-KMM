e3184433c82b7831dd962aac89e21f0da9d6334d
Mitch Tabian <mitch@tabian.ca>
2021-05-17 09:49:03 -0700

    provide SearchRecipes.kt use case with Hilt and test

---
+++ androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/di/InteractorsModule.kt
@@ -0,0 +1,26 @@
+package com.codingwithmitch.food2forkkmm.android.di
+
+import com.codingwithmitch.food2forkkmm.datasource.network.RecipeService
+import com.codingwithmitch.food2forkkmm.interactors.recipe_list.SearchRecipes
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object InteractorsModule {
+
+    @Singleton
+    @Provides
+    fun provideSearchRecipes(
+        recipeService: RecipeService
+    ): SearchRecipes{
+        return SearchRecipes(recipeService = recipeService)
+    }
+}
+
+
+
+
--- androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/presentation/recipe_list/RecipeListViewModel.kt
+++ androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/presentation/recipe_list/RecipeListViewModel.kt
@@ -2,7 +2,12 @@ package com.codingwithmitch.food2forkkmm.android.presentation.recipe_list
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.codingwithmitch.food2forkkmm.interactors.recipe_list.SearchRecipes
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.forEach
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,9 +15,31 @@ class RecipeListViewModel
 @Inject
 constructor(
     private val savedStateHandle: SavedStateHandle, // don't need for this VM
+    private val searchRecipes: SearchRecipes,
 ): ViewModel() {
 
+    init {
+        loadRecipes()
+    }
+
+    private fun loadRecipes(){
+        searchRecipes.execute(
+            page = 1,
+            query = "chicken"
+        ).onEach { dataState ->
+            println("RecipeListVM: ${dataState.isLoading}")
+
+            dataState.data?.let { recipes ->
+                println("RecipeListVM: recipes: ${recipes}")
+            }
+
+            dataState.message?.let { message ->
+                println("RecipeListVM: error: ${message}")
+            }
+        }.launchIn(viewModelScope)
+    }
 }
+
 
 
 
