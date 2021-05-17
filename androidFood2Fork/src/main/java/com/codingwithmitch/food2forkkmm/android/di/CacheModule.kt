ce3e32509bd30df652947b4a4cbb93657292cf81
Mitch Tabian <mitch@tabian.ca>
2021-05-17 11:47:28 -0700

    provide RecipeCacheService with hilt

---
+++ androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/di/CacheModule.kt
@@ -0,0 +1,33 @@
+package com.codingwithmitch.food2forkkmm.android.di
+
+import com.codingwithmitch.food2forkkmm.android.BaseApplication
+import com.codingwithmitch.food2forkkmm.datasource.cache.*
+import com.codingwithmitch.food2forkkmm.domain.util.DatetimeUtil
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CacheModule {
+
+    @Singleton
+    @Provides
+    fun provideRecipeDatabase(context: BaseApplication): RecipeDatabase {
+        return RecipeDatabaseFactory(driverFactory = DriverFactory(context)).createDatabase()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRecipeCache(
+        recipeDatabase: RecipeDatabase,
+        datetimeUtil: DatetimeUtil,
+    ): RecipeCache {
+        return RecipeCacheImpl(
+            recipeDatabase = recipeDatabase,
+            datetimeUtil = datetimeUtil,
+        )
+    }
+}
