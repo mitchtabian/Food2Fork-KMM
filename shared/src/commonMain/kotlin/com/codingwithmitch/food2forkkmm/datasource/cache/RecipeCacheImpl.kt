d5d83a291f15a7326f232ad8b15c717625a72914
Mitch Tabian <mitch@tabian.ca>
2021-05-17 11:44:20 -0700

    Add Recipe_Entity mappers

--- shared/src/commonMain/kotlin/com/codingwithmitch/food2forkkmm/datasource/cache/RecipeCacheImpl.kt
+++ shared/src/commonMain/kotlin/com/codingwithmitch/food2forkkmm/datasource/cache/RecipeCacheImpl.kt
@@ -19,7 +19,7 @@ class RecipeCacheImpl(
             featured_image = recipe.featuredImage,
             rating = recipe.rating.toLong(),
             source_url = recipe.sourceUrl,
-            ingredients = , // TODO("Convert String to List<String>")
+            ingredients = recipe.ingredients.convertIngredientListToString(),
             date_updated = datetimeUtil.toEpochMilliseconds(recipe.dateUpdated),
             date_added = datetimeUtil.toEpochMilliseconds(recipe.dateAdded),
         )
@@ -36,21 +36,21 @@ class RecipeCacheImpl(
             query = query,
             pageSize = RECIPE_PAGINATION_PAGE_SIZE.toLong(),
             offset = (page * RECIPE_PAGINATION_PAGE_SIZE).toLong()
-        ).executeAsList() // TODO("convert List<Recipe_Entity> to List<Recipe>")
+        ).executeAsList().toRecipeList()
     }
 
     override fun getAll(page: Int): List<Recipe> {
         return queries.getAllRecipes(
             pageSize = RECIPE_PAGINATION_PAGE_SIZE.toLong(),
             offset = (page * RECIPE_PAGINATION_PAGE_SIZE).toLong()
-        ).executeAsList() // TODO("convert List<Recipe_Entity> to List<Recipe>")
+        ).executeAsList().toRecipeList()
     }
 
     override fun get(recipeId: Int): Recipe? {
         return try {
             queries
                 .getRecipeById(id = recipeId.toLong())
-                .executeAsOne() // TODO("convert Recipe_Entity to Recipe")
+                .executeAsOne().toRecipe()
         }catch (e: NullPointerException){
             null
         }
--- shared/src/commonMain/kotlin/com/codingwithmitch/food2forkkmm/datasource/cache/RecipeDatabaseFactory.kt
+++ shared/src/commonMain/kotlin/com/codingwithmitch/food2forkkmm/datasource/cache/RecipeDatabaseFactory.kt
@@ -1,5 +1,6 @@
 package com.codingwithmitch.food2forkkmm.datasource.cache
 
+import com.codingwithmitch.food2forkkmm.domain.model.Recipe
 import com.squareup.sqldelight.db.SqlDriver
 
 class RecipeDatabaseFactory(
@@ -12,4 +13,42 @@ class RecipeDatabaseFactory(
 
 expect class DriverFactory {
     fun createDriver(): SqlDriver
+}
+
+fun Recipe_Entity.toRecipe(): Recipe {
+    val datetimeUtil = DatetimeUtil()
+    return Recipe(
+        id = id.toInt(),
+        title = title,
+        publisher = publisher,
+        featuredImage = featured_image,
+        rating = rating.toInt(),
+        sourceUrl = source_url,
+        ingredients = ingredients.convertIngredientsToList(),
+        dateAdded = datetimeUtil.toLocalDate(date_added),
+        dateUpdated = datetimeUtil.toLocalDate(date_updated),
+    )
+}
+
+fun List<Recipe_Entity>.toRecipeList(): List<Recipe>{
+    return map{it.toRecipe()}
+}
+
+/**
+ * "Carrot, potato, Chicken, ..."
+ */
+fun List<String>.convertIngredientListToString(): String {
+    val ingredientsString = StringBuilder()
+    for(ingredient in this){
+        ingredientsString.append("$ingredient,")
+    }
+    return ingredientsString.toString()
+}
+
+fun String.convertIngredientsToList(): List<String>{
+    val list: ArrayList<String> = ArrayList()
+    for(ingredient in split(",")){
+        list.add(ingredient)
+    }
+    return list
 }
