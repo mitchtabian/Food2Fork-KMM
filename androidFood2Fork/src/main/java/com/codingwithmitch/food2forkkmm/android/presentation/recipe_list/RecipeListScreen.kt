d0d48319d1d37dadcc12895e7b8560af43b0f6b9
Mitch Tabian <mitch@tabian.ca>
2021-05-21 13:13:51 -0700

    Begin building SearchAppBar.kt

--- androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/presentation/recipe_list/RecipeListScreen.kt
+++ androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/presentation/recipe_list/RecipeListScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.codingwithmitch.food2forkkmm.android.presentation.recipe_list.components.GradientDemo
 import com.codingwithmitch.food2forkkmm.android.presentation.recipe_list.components.RecipeList
+import com.codingwithmitch.food2forkkmm.android.presentation.recipe_list.components.SearchAppBar
 import com.codingwithmitch.food2forkkmm.android.presentation.theme.AppTheme
 import com.codingwithmitch.food2forkkmm.presentation.recipe_list.RecipeListEvents
 import com.codingwithmitch.food2forkkmm.presentation.recipe_list.RecipeListState
@@ -28,15 +30,29 @@ fun RecipeListScreen(
     AppTheme(
         displayProgressBar = state.isLoading
     ) {
-        RecipeList(
-            loading = state.isLoading,
-            recipes = state.recipes,
-            page = state.page,
-            onTriggerNextPage = {
-                onTriggerEvent(RecipeListEvents.NextPage)
+        Scaffold(
+            topBar = {
+                SearchAppBar(
+                    query = state.query,
+                    onQueryChanged = {
+                        // TODO("Update state")
+                    },
+                    onExecuteSearch = {
+                        // TODO("Execute a new search")
+                    },
+                )
             },
-            onClickRecipeListItem = onSelectRecipe
-        )
+        ) {
+            RecipeList(
+                loading = state.isLoading,
+                recipes = state.recipes,
+                page = state.page,
+                onTriggerNextPage = {
+                    onTriggerEvent(RecipeListEvents.NextPage)
+                },
+                onClickRecipeListItem = onSelectRecipe
+            )
+        }
     }
 }
 
---
+++ androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/presentation/recipe_list/components/SearchAppBar.kt
@@ -0,0 +1,72 @@
+package com.codingwithmitch.food2forkkmm.android.presentation.recipe_list.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+@ExperimentalComposeUiApi
+@Composable
+fun SearchAppBar(
+    query: String,
+    onQueryChanged: (String) -> Unit,
+    onExecuteSearch: () -> Unit,
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        color = MaterialTheme.colors.secondary,
+        elevation = 8.dp,
+    ) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth(.9f)
+                        .padding(8.dp)
+                    ,
+                    value = query,
+                    onValueChange = { onQueryChanged(it) },
+                    label = { Text(text = "Search") },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Done,
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            onExecuteSearch()
+                            keyboardController?.hide()
+                        },
+                    ),
+                    leadingIcon = { Icon(Icons.Filled.Search, contentDescription = "Search Icon") },
+                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                    colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface),
+                )
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
