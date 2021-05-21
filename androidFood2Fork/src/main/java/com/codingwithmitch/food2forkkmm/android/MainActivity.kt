b955b29549c83bf7fb1a497dcc6c5dc2d61cc56e
Mitch Tabian <mitch@tabian.ca>
2021-05-21 09:42:44 -0700

    forgot annotations

--- androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/MainActivity.kt
+++ androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/MainActivity.kt
@@ -3,11 +3,15 @@ package com.codingwithmitch.food2forkkmm.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.ExperimentalComposeUiApi
 import com.codingwithmitch.food2forkkmm.android.presentation.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @ExperimentalMaterialApi
+    @ExperimentalComposeUiApi
     @ExperimentalStdlibApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
