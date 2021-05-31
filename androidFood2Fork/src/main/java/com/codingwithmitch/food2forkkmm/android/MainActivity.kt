bc8dd7eac4ec60929fbefc1e7b5137f667ef6f20
Mitch Tabian <mitch@tabian.ca>
2021-05-31 10:37:06 -0700

    queue data structure for kotlin

--- androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/MainActivity.kt
+++ androidFood2Fork/src/main/java/com/codingwithmitch/food2forkkmm/android/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.codingwithmitch.food2forkkmm.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.ExperimentalComposeUiApi
 import com.codingwithmitch.food2forkkmm.android.presentation.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.InternalCoroutinesApi
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
