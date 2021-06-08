991ba206638a908452703ff1d2d52bd198a20801
Mitch Tabian <mitch@tabian.ca>
2021-06-08 11:35:55 -0700

    Updating the RecipeCard.swift

--- iosFood2Fork/iosFood2Fork/presentation/components/RecipeCard.swift
+++ iosFood2Fork/iosFood2Fork/presentation/components/RecipeCard.swift
@@ -11,18 +11,38 @@ struct RecipeCard: View {
     }
     
     var body: some View {
-        WebImage(url: URL(string: recipe.featuredImage))
-            .resizable()
-            .placeholder(Image(systemName: "photo")) // Placeholder Image
-            .placeholder {
-                Rectangle().foregroundColor(.white)
+            VStack(alignment: .leading){
+                WebImage(url: URL(string: recipe.featuredImage))
+                            .resizable()
+                            .placeholder(Image(systemName: "photo")) // Placeholder Image
+                            .placeholder {
+                                Rectangle().foregroundColor(.white)
+                            }
+                            .indicator(.activity)
+                            .transition(.fade(duration: 0.5))
+                            .scaledToFill() // 1
+                            .frame(height: 250, alignment: .center) // 2
+                            .clipped() // 3
+                    
+                HStack(alignment: .lastTextBaseline){
+                    Text(recipe.title)
+                        .font(.body)
+                        .frame(alignment: .center)
+                    
+                    Spacer()
+                    
+                    Text(String(recipe.rating))
+                        .frame(alignment: .trailing)
+                }
+                .padding(.top, 8)
+                .padding(.leading, 8)
+                .padding(.trailing, 8)
+                .padding(.bottom, 12)
             }
-            .indicator(.activity)
-            .transition(.fade(duration: 0.5))
-            .scaledToFill() // 1
-            .frame(height: 250, alignment: .center) // 2
-            .clipped() // 3
-    }
+            .background(Color.white)
+            .cornerRadius(8)
+            .shadow(radius: 5)
+        }
 }
 
 struct RecipeCard_Previews: PreviewProvider {
